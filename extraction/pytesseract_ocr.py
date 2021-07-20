686b0fbce7e7f55e022e8b2c56c318c04feebbe6
Fauzan Taufik <fauzantaufik178@gmail.com>
2021-07-20 08:27:35 +0700

    rmv

--- extraction/pytesseract_ocr.py
+++ extraction/pytesseract_ocr.py
@@ -69,7 +69,10 @@ def combine_lines_and_bb(x):
     and return in as pandas series
     
     """
-    return pd.Series(dict(left=x['left'].min(), top=x['top'].min(), width=x['width'].max(), height=x['height'].sum(), lines_combine="%s" % ' '.join(x['text'])))
+    return pd.Series(dict(
+        left=x['left'].min(), top=x['top'].min(), width=x['width'].max(), 
+        height=x['height'].sum(), lines_combine="%s" % ' '.join(x['text'])
+        ))
 
 def combine_lines_comma_separated_and_bb(x):
     """Combine row of column lines_combine with comma separated, 
@@ -80,151 +83,8 @@ def combine_lines_comma_separated_and_bb(x):
     and return in as pandas series
     
     """
-    return pd.Series(dict(left=x['left'].min(), top=x['top'].min(), width=x['width'].max(), height=x['height'].sum(), lines_combine="%s" % ', '.join(x['lines_combine'])))
-
-def pytesseract_ocr(img_ori, img_gray, height_, width_, table_, nontable_, undetected_bbox):
-    """Return the string result from ocr outside images each page per line.
-
-    Parameters:
-    -----------
-    img_ori : list of integer 
-        return from cv2.imread
-    img_gray : list of integer 
-        return from cv2.imread grayscale
-    height_ : int 
-        height of pixel from page
-    width_ : int
-        width of pixel from page
-    table_ : list of list of integer 
-        contain boundingboxes of table detected with format of [[x1,y1,w1,h1],[x2,y2,w2,h2],...]
-    nontable_ : list of list of integer 
-        contain boundingboxes of nontable detected with format of [[x1,y1,w1,h1],[x2,y2,w2,h2],...]
-    undetected_bbox : list of list of integer 
-        contain boundingboxes of bbox of images that not detected as a table or nontable with format of [[x1,y1,w1,h1],[x2,y2,w2,h2],...]
-    pyteserract_dir : str
-        directory of pytesseract
-
-    Returns:
-    df_combined_final : dataframe 
-        with columns (left (int):bbox left, top (int):bbox top, width(int):bbox width, height(int):bbox height, lines_combine (str): sentences from each line, detected_as (str) : detected as "text" or "title", group (int):group from each sentences)
-    """ 
-
-    img = img_ori
-    img2 = img_gray
-    img3 = cv2.medianBlur(img, 5)
-    d2 = pytesseract.image_to_data(img3, output_type=Output.DATAFRAME)
-    debug_dataframe = pd.DataFrame(data=None, columns=d2.columns)
-    tables = table_+nontable_+undetected_bbox
-    tables2 = []
-    df_tables = d2[
-        (d2['height'] < (height_)) & (d2['height'] > (height_//15)) & 
-        (d2['width'] > (width_//15)) & (d2['width'] < width_) & 
-        (d2['conf'] != -1)
-        ]
-    for index, row in df_tables.iterrows():
-        tables2.append([row.left, row.top, row.width, row.height])
-    df_text = d2[(d2['conf']) != -1].reset_index(drop=True)
-    df_text["text"] = df_text["text"].astype(str)
-    extracted_text = []
-    extracted_text.append([])
-    first_row = True
-    count = 0
-    for index, row in df_text.iterrows():
-        if(len(tables) == 0):
-            if first_row:
-                extracted_text[count].append(row.text)
-                debug_dataframe = debug_dataframe.append(
-                    row, ignore_index=True)
-                previous_x = row.top
-                first_row = False
-            else:
-                if(previous_x-(height_//70) < row.top < previous_x+(height_//70)):
-                    extracted_text[count].append(row.text)
-                    previous_x = row.top
-                    debug_dataframe = debug_dataframe.append(
-                        row, ignore_index=True)
-                else:
-                    count = count+1
-                    extracted_text.append([])
-                    extracted_text[count].append(row.text)
-                    previous_x = row.top
-                    debug_dataframe = debug_dataframe.append(
-                        row, ignore_index=True)
-        else:
-            if ((not (inside_box_all(tables, [row.left, row.top, row.width, row.height]))) and (len(row.text) > 1)):
-                if first_row:
-                    extracted_text[count].append(row.text)
-                    debug_dataframe = debug_dataframe.append(row, ignore_index=True)
-                    previous_x = row.top
-                    first_row = False
-                else:
-                    if(previous_x-(height_//70) < row.top < previous_x+(height_//70)):
-                        extracted_text[count].append(row.text)
-                        previous_x = row.top
-                        debug_dataframe = debug_dataframe.append(row, ignore_index=True)
-                    else:
-                        count = count+1
-                        extracted_text.append([])
-                        extracted_text[count].append(row.text)
-                        previous_x = row.top
-                        debug_dataframe = debug_dataframe.append(row, ignore_index=True)
-    if len(debug_dataframe) > 0:
-        df_combined_sentences_bb = debug_dataframe.groupby(by = ["block_num","par_num",'line_num']).apply(combine_lines_and_bb)
-        df_combined_sentences_bb = df_combined_sentences_bb.reset_index()
-        df_combined_sentences_bb['lines'] = (df_combined_sentences_bb.index) + 1
-        df_combined_sentences_bb = df_combined_sentences_bb[['lines','left','top','width','height','lines_combine']]
-        df_combined_sentences_bb = (df_combined_sentences_bb.sort_values(by=['top'])).reset_index(drop=True)
-        df_combined_sentences_bb['lines_par'] = 0
-        lines_par_count = 1
-        bbox_text = []
-        blur = cv2.GaussianBlur(img2, (7, 7), 0)
-        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-        dilate = cv2.dilate(thresh, kernel, iterations=20)
-        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        count_ = []      
-        for c in cnts:
-            check = True
-            x, y, w, h = cv2.boundingRect(c) 
-            count_.append([x, y, w, h])
-        df_bbox = pd.DataFrame(count_, columns=['y','x','w','h'])
-        df_bbox = (df_bbox.sort_values(by=['x'])).reset_index(drop=True)
-        for index1,row1 in df_bbox.iterrows() :  
-            y, x, w, h = [row1.y,row1.x,row1.w,row1.h] 
-            for index, row in df_combined_sentences_bb.iterrows():
-                if row.lines_par == 0 and get_intersection([y, x, w, h],[row['left'], row['top'], row['width'], row['height']]) > 0:
-                    df_combined_sentences_bb.loc[index,'lines_par'] = lines_par_count
-            lines_par_count+=1
-        list_df_text_grouped = []
-        for key, df_lines_par in df_combined_sentences_bb.groupby('lines_par'):
-            df_lines_par = df_lines_par.reset_index(drop=True)
-            df_lines_par['lowercase_tag'] = 0
-            count_lowercase_tag = 0
-            for index, row in (df_lines_par).iterrows():
-                if (index > 0):
-                    if row.lines_combine[0].islower():
-                        df_lines_par.loc[index,'lowercase_tag'] = count_lowercase_tag
-                    else:
-                        count_lowercase_tag += 1
-                        df_lines_par.loc[index,'lowercase_tag'] = count_lowercase_tag
-            df_lines_par = df_lines_par.groupby('lowercase_tag').apply(combine_lines_comma_separated_and_bb)
-            df_lines_par = df_lines_par.reset_index()
-            if (len(df_lines_par) == 1):
-                df_lines_par['detected_as'] = 'text'
-            else:
-                df_lines_par['detected_as'] = 'text'
-                df_lines_par.loc[0, 'detected_as'] = 'title'
-            df_lines_par = df_lines_par.drop(columns='lowercase_tag')
-            list_df_text_grouped.append(df_lines_par)
-        for i in range(len(list_df_text_grouped)):
-            list_df_text_grouped[i]['group'] = i+1
-        if len(list_df_text_grouped)>0:
-            df_combined_final = pd.concat(list_df_text_grouped)
-        else:
-            df_combined_final = pd.DataFrame(columns = ['left','top','width','height','lines_combine','detected_as','group'])
-    else:
-        df_combined_sentences_bb = []
-        list_df_text_grouped = []
-        df_combined_final = []
-    return(df_combined_final)
+    return pd.Series(
+        dict(
+        left=x['left'].min(), top=x['top'].min(), 
+        width=x['width'].max(), height=x['height'].sum(), 
+        lines_combine="%s" % ', '.join(x['lines_combine'])))
